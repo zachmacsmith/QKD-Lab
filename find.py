c198b14503f2dcf05fbb74940cf6d89aebff84b1
Zach Macsmith <zachmacsmith@gmail.com>
2026-04-14 18:09:07 -0500

    find3

--- find.py
+++ find.py
@@ -8,3 +8,16 @@ print("Folder exists:", p.exists())
 print("\nAll DLLs:")
 for dll in sorted(p.glob("*.dll")):
     print(dll.name)
+
+    import pathlib
+
+# Search common install locations
+search_roots = [
+    r"C:\Program Files",
+    r"C:\Program Files (x86)",
+    r"C:\ProgramData",
+]
+
+for root in search_roots:
+    for p in pathlib.Path(root).rglob("Thorlabs.MotionControl.DeviceManagerCLI.dll"):
+        print(p.parent)
